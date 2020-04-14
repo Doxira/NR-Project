e1048c82dc40c75a623142f95b515c6999b2f989
Doxira <Doxira@abv.bg>
2020-04-14 16:01:22 +0300

    Adding JobCategorySeeder
    Adding JobCategorySeeder and JobController

---
+++ Data/NRProject.Data/Seeding/JobCategoriesSeeder.cs
@@ -0,0 +1,29 @@
+﻿namespace NRProject.Data.Seeding
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using NRProject.Data.Common.Models;
+
+    public class JobCategoriesSeeder : ISeeder
+    {
+        public async Task SeedAsync(ApplicationDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.JobCategories.Any())
+            {
+                return;
+            }
+
+            var jobCategories = new List<string> { "Auto Repair", "House Maintance", "Gardening", "Photography", "Others" };
+            foreach (var jobCategory in jobCategories)
+            {
+                 dbContext.JobCategories.AddAsync(new JobCategory
+                 {
+                    Name = jobCategory,
+                 });
+            }
+        }
+    }
+}
---
+++ Web/NRProject.Web/Controllers/JobsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NRProject.Web.Controllers
+{
+    public class JobsController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return this.View();
+        }
+    }
+}
