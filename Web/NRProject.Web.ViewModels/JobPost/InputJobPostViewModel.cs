14b85b454828eacb169716cbaa1a387c4c809d23
Doxira <Doxira@abv.bg>
2020-04-30 11:26:29 +0300

    Adding Validations

--- Web/NRProject.Web.ViewModels/JobPost/InputJobPostViewModel.cs
+++ Web/NRProject.Web.ViewModels/JobPost/InputJobPostViewModel.cs
@@ -7,10 +7,13 @@
 
     public class InputJobPostViewModel : IMapFrom<JobPosts>
     {
+        [Required]
+        [MaxLength(30)]
         public string Title { get; set; }
-
+        [Required]
+        [MaxLength(1000)]
         public string Content { get; set; }
-
+        [Required]
         public virtual int JobCategoryId { get; set; }
     }
 }
